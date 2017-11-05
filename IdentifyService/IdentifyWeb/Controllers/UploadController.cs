b67a838f25eaf19d6eaf4a7c0f74e32203956ce6
eunji <Eunji>
2017-11-05 21:41:28 +0900

    api web update

--- IdentifyService/IdentifyWeb/Controllers/UploadController.cs
+++ IdentifyService/IdentifyWeb/Controllers/UploadController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,12 +9,25 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Queue; // Namespace for Queue storage types
+
 namespace IdentifyWeb.Controllers
 {
     public class UploadController : ApiController
     {
         public async Task<HttpResponseMessage> PostFormData()
         {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference("ocr");
+            queue.CreateIfNotExists();
+
             // Check if the request contains multipart/form-data.
             if (!Request.Content.IsMimeMultipartContent())
             {
@@ -23,6 +37,8 @@ namespace IdentifyWeb.Controllers
             string root = HttpContext.Current.Server.MapPath("~/App_Data");
             var provider = new MultipartFormDataStreamProvider(root);
 
+
+            //사진 파일을 App_Data 폴더 밑에 임시로 저장
             try
             {
                 // Read the form data.
@@ -33,6 +49,18 @@ namespace IdentifyWeb.Controllers
                 {
                     Trace.WriteLine(file.Headers.ContentDisposition.FileName);
                     Trace.WriteLine("Server file path: " + file.LocalFileName);
+
+                    var client = new RestClient("https://eastasia.api.cognitive.microsoft.com/vision/v1.0/ocr?language=ko&detectOrientation=true");
+                    var request = new RestRequest(Method.POST);
+
+                    request.AddHeader("content-type", "application/octect-stream");
+                    request.AddHeader("ocp-apim-subscription-key", "caec3ca99dc94429ba2e390d8fdd5964");
+                    request.AddFile("test.png", file.LocalFileName);
+                    IRestResponse response = client.Execute(request);
+                    Trace.WriteLine(response.Content);
+
+                    CloudQueueMessage message = new CloudQueueMessage(response.Content);
+                    queue.AddMessage(message);
                 }
                 return Request.CreateResponse(HttpStatusCode.OK);
             }
@@ -40,6 +68,9 @@ namespace IdentifyWeb.Controllers
             {
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e);
             }
+
+            
+
         }
 
     }
